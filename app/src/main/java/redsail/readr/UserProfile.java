d361184048eeaf59f47e9d2d43a29dfce90aadd6
Celine Leung <sn.leung@hotmail.com>
2018-07-09 10:19:34 +0800

    userprofile

--- app/src/main/java/redsail/readr/UserProfile.java
+++ app/src/main/java/redsail/readr/UserProfile.java
@@ -1,21 +1,23 @@
 package redsail.readr;
 
+import android.app.usage.UsageEvents;
 import com.google.firebase.database.IgnoreExtraProperties;
+import java.util.*;
+import java.time.*;
 
 @IgnoreExtraProperties
 public class UserProfile {
-
     public ArrayList<Book> collection;
     public String UID;
     public String name;
-    public String bio = New String("Write something about yourself");
+    public String bio = new String("Write something about yourself");
     public int currency;
     public LocalDate birthday;
     public String communityRole;
     public String occupation;
-    public ArrayList<Achievement> achievements;
-    public ArrayList<Converstion> converstions;
-    public ArrayList<Event> eventHistory;
+//    public ArrayList<Achievement> achievements;
+//    public ArrayList<Conversation> conversations;
+    public ArrayList<UsageEvents.Event> eventHistory;
     public ArrayList<Book> wishList;
     public ArrayList<Book> recommendations;
     public ArrayList<String> notes;
