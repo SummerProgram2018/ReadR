9ff25988787fc0b69b888855f47e77e3e236a64c
Celine Leung <sn.leung@hotmail.com>
2018-07-09 16:17:19 +0800

    trade edited

--- app/src/main/java/redsail/readr/BuyActivity.java
+++ app/src/main/java/redsail/readr/BuyActivity.java
@@ -9,6 +9,7 @@ public class BuyActivity extends BaseActivity implements View.OnClickListener {
     private static final String TAG = "TradeActivity";
 
     private Button mConfirmButton;
+    private Button mCancelButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,18 +17,24 @@ public class BuyActivity extends BaseActivity implements View.OnClickListener {
 
         // Views
         mConfirmButton = (Button) findViewById(R.id.button_confirm_purchase);
-
+        mCancelButton = (Button) findViewById(R.id.button_cancel_purchase);
+        // Click listeners
+        mConfirmButton.setOnClickListener(this);
     }
 
     private void confirmPurchase() {
         Log.d(TAG, "BookPurchase");
     }
-
+    private void cancelPurchase() {
+        Log.d(TAG, "PurchaseCancel");
+    }
 
     public void onClick(View v) {
         int i = v.getId();
         if (i == R.id.button_confirm_purchase) {
             confirmPurchase();
+        } else if (i == R.id.button_cancel_purchase) {
+            cancelPurchase();
         }
     }
 }
